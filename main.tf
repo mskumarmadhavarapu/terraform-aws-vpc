37e6303358e05966b1d9268ce5ea9a5f77f16103
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-03 22:51:48 +0530

    functions

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
-resource "aws_instance" "this" {
-    ami                 = var.ami_id
-    instance_type       = var.instance_type
-    vpc_security_group_ids = var.sg_ids
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tendancy = "default"
+  enable_dns_hostnames = true
 
-    tags = local.vpc_final_tags
-}    
+  tags = {
+    Name = "manual-vpc"
+  }
+}
