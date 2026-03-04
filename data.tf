d9f4cfde54c649936d4472fd98d4417fb8f9b2d9
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-05 00:08:55 +0530

    functions

--- data.tf
+++ data.tf
@@ -7,7 +7,7 @@ data "aws_vpc" "default" {
 }
 
 data "aws_route_table" "default" {
-  vpc_id = data.aws_vpc.main.id
+  vpc_id = data.aws_vpc.default.id
   filter {
     name   = "association.main"
     values = ["true"]
